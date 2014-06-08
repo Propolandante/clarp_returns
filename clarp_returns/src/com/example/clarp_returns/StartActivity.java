fb0c4194a7d72df1d19aa00d945404b1db912cd4
Derek Donahue <propolandante@gmail.com>
2014-06-07 17:10:11 -0700

    for britbrit

--- clarp_returns/src/com/example/clarp_returns/StartActivity.java
+++ clarp_returns/src/com/example/clarp_returns/StartActivity.java
@@ -2,6 +2,7 @@ package com.example.clarp_returns;
 
 import java.util.ArrayList;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.facebook.Request;
@@ -23,6 +25,7 @@ import com.facebook.SessionState;
 import com.facebook.model.GraphUser;
 
 public class StartActivity extends ActionBarActivity {
+	
 	
 	private ListView gamesListView;
     private ArrayList<Game> gamesList;
@@ -84,8 +87,8 @@ public class StartActivity extends ActionBarActivity {
         super.onResume();
 
         gamesListView = (ListView) findViewById(R.id.games_list_view);
-        gamesListView.setEmptyView(findViewById(R.id.empty_games_view));
-
+        //gamesListView.setEmptyView(findViewById(R.id.empty_games_view));
+        
         gamesListView.setOnItemClickListener(new OnItemClickListener() {
 
             // user clicks to go to a select screen where they can choose
@@ -102,7 +105,7 @@ public class StartActivity extends ActionBarActivity {
             }
         });
 
-        //Game myGame = new Game("MyGame", 0, null, null);
+        Game myGame = new Game("MyGame", 0, null, null);
 
         gamesList = new ArrayList<Game>();
         //gamesList.add(0, myGame);

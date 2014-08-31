7c54860eceb4a2213775922796e2b36ae638f50c
Brittany West <bkwest@ucsc.edu>
2014-08-30 18:44:27 -0700

    Yay the card list works. Removed files that were from other attempts to make it work.

--- clarp_returns/src/com/example/clarp_returns/CardListActivity.java
+++ clarp_returns/src/com/example/clarp_returns/CardListActivity.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,100 +11,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
-import com.parse.FindCallback;
-import com.parse.GetDataCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-
 
 public class CardListActivity extends Activity {
 
     // result codes
     static final int NEW_CARD = 0;
 
-    //private CardViewAdapter mainAdapter;
-    //private ParseQueryAdapter<ClarpCard> mainAdapter;
-
-    ListView cardListView;
-    List<ParseObject> ob;
-    ProgressDialog cardProgressDialog;
-    CardListViewAdapter cardAdapter;
-    private List<ClarpCard> cardList = null;
-
+    private ListView cardListView;
+    private List<ClarpCard> cardList;
+    private CardQueryAdapter cardAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //mainAdapter = new CardViewAdapter(this);
-        //mainAdapter = new ParseQueryAdapter<ClarpCard>(this, ClarpCard.class);
-        //setListAdapter(mainAdapter);
-
         setContentView(R.layout.activity_card_list);
 
         cardListView = (ListView) findViewById(R.id.card_list_view);
 
-        //new RemoteDataTask().execute();
         cardList = new ArrayList<ClarpCard>();
-        cardAdapter = new CardListViewAdapter(CardListActivity.this, cardList);
+
+        cardAdapter = new CardQueryAdapter(CardListActivity.this);
         cardListView.setAdapter(cardAdapter); // this apparently causes a NPE
-
-
-        ParseQuery<ClarpCard> query = ParseQuery.getQuery("ClarpCard");
-        query.findInBackground(new FindCallback<ClarpCard>() {
-            @Override
-            public void done(List<ClarpCard> objects, ParseException e) {
-                Log.d(ClarpApplication.TAG, "Trying card retrieval");
-                if (e == null) {
-
-                    Log.d(ClarpApplication.TAG, "Card retrieval successful");
-
-                    for(ClarpCard card: objects) {
-
-                        ParseFile image = (ParseFile) card.get("photo");
-
-                        ClarpCard clarpCard = new ClarpCard();
-                        clarpCard.setCardType(Integer.toString(0)); // will need to change
-                        clarpCard.setCardName((String) card.get("cardName"));
-
-                        if(image != null) {
-
-                            Log.d(ClarpApplication.TAG, "Image is not null");
-                            image.getDataInBackground(new GetDataCallback() {
-                                @Override
-                                public void done(byte[] data, ParseException e) {
-
-                                    if (e == null) {
-                                        Log.d(ClarpApplication.TAG, "We've got data in data.");
-                                    } else {
-                                        Log.d(ClarpApplication.TAG, "There was a problem downloading data");
-                                    }
-                                }
-                            });
-
-                            clarpCard.setPhotoFile(image);
-                            cardList.add(clarpCard);
-
-                        } else {
-
-                            Log.d(ClarpApplication.TAG, "Image is null"); // this happens a lot
-                            //clarpCard.setPhotoFile((ParseFile) getResources().getDrawable(R.drawable.jordan));
-                        }
-                    }
-                } else {
-
-                    Log.d(ClarpApplication.TAG, "Card retrieval failed");
-                    //objectRetrievalFailed();
-                    Log.e("Error", e.getMessage());
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        cardAdapter.notifyDataSetChanged();
+        //cardAdapter.loadObjects();
+        updateCardList();
 
     }
 
@@ -144,9 +74,8 @@ public class CardListActivity extends Activity {
     }
 
     private void updateCardList() {
-        //mainAdapter.loadObjects();
+        cardAdapter.loadObjects();
         Log.v(ClarpApplication.TAG, "Card objects loaded");
-        //setListAdapter(mainAdapter);
     }
 
     private void newClarpCard() {
@@ -164,45 +93,6 @@ public class CardListActivity extends Activity {
             Log.v(ClarpApplication.TAG, "Card List updated");
         }
     }
-
-    private void startTask() {
-        cardProgressDialog = new ProgressDialog(CardListActivity.this);
-        cardProgressDialog.setTitle("Card ListView");
-
-        cardProgressDialog.setMessage("Loading...");
-        cardProgressDialog.setIndeterminate(false);
-        cardProgressDialog.show();
-    }
-
-    private void endTask() {
-        cardListView = (ListView) findViewById(R.id.card_list_view);
-        cardAdapter = new CardListViewAdapter(CardListActivity.this, cardList);
-        cardListView.setAdapter(cardAdapter);
-        cardProgressDialog.dismiss();
-    }
-
-    //    private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
-    //        @Override
-    //        protected void onPreExecute() {
-    //            super.onPreExecute();
-    //            cardProgressDialog = new ProgressDialog(CardListActivity.this);
-    //            cardProgressDialog.setTitle("Card ListView");
-    //
-    //            cardProgressDialog.setMessage("Loading...");
-    //            cardProgressDialog.setIndeterminate(false);
-    //            cardProgressDialog.show();
-    //        }
-    //
-    //
-    //        @Override
-    //        protected Void doInBackground(Void... params) {
-    //            // TODO Auto-generated method stub
-    //            return null;
-    //
-    //
-    //
-    //        }
-    //    }
 }
 
 
---
+++ clarp_returns/src/com/example/clarp_returns/CardQueryAdapter.java
@@ -0,0 +1,55 @@
+package com.example.clarp_returns;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.parse.ParseFile;
+import com.parse.ParseImageView;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+
+public class CardQueryAdapter extends ParseQueryAdapter<ClarpCard>{
+
+    public CardQueryAdapter(Context context) {
+
+        super(context, new ParseQueryAdapter.QueryFactory<ClarpCard>() {
+            @Override
+            public ParseQuery create() {
+                ParseQuery query = new ParseQuery("ClarpCard");
+                return query;
+            }
+        });
+    }
+
+    // Customize the layout by overriding getItemView
+    @Override
+    public View getItemView(ClarpCard card, View v, ViewGroup parent) {
+        if (v == null) {
+            v = View.inflate(getContext(), R.layout.cardlistview_item, null);
+        }
+
+
+        super.getItemView(card, v, parent);
+
+        // add and download the image
+        ParseImageView cardImage = (ParseImageView) v.findViewById(R.id.cardImage);
+        ParseFile imageFile = card.getPhotoFile();
+        if (imageFile != null) {
+            cardImage.setParseFile(imageFile);
+            cardImage.loadInBackground();
+        }
+
+        // add the name and type texts
+
+        TextView nameTextView = (TextView) v.findViewById(R.id.cardName);
+        nameTextView.setText(card.getCardName());
+
+        TextView typeTextView = (TextView) v.findViewById(R.id.cardType);
+        typeTextView.setText(card.getCardType());
+
+        return v;
+    }
+
+}

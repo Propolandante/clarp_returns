7f3f41de926031b2036cc830bf12ce29a017873e
Brittany West <bkwest@ucsc.edu>
2014-09-02 19:59:27 -0700

    added some new behavior to push notifications, the beginnings of being able to accept an invitation

---
+++ clarp_returns/src/com/example/clarp_returns/InviteReceiver.java
@@ -0,0 +1,33 @@
+package com.example.clarp_returns;
+
+import java.util.Iterator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class InviteReceiver extends BroadcastReceiver{
+    private static final String TAG = "InviteReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        try {
+            String action = intent.getAction();
+            //String channel = intent.getExtras().getString("com.parse.Channel");
+            JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+
+            Log.d(TAG, "got action " + action + " with:");
+            Iterator itr = json.keys();
+            while (itr.hasNext()) {
+                String key = (String) itr.next();
+                Log.d(TAG, "..." + key + " => " + json.getString(key));
+            }
+        } catch (JSONException e) {
+            Log.d(TAG, "JSONException: " + e.getMessage());
+        }
+    }
+}

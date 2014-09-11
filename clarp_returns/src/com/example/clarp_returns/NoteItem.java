3c17e16ecf48fdedc11427b9eb4038b36831805e
Brittany West <bkwest@ucsc.edu>
2014-09-10 21:17:16 -0700

    The beginnings of saving checkbox states

--- clarp_returns/src/com/example/clarp_returns/NoteItem.java
+++ clarp_returns/src/com/example/clarp_returns/NoteItem.java
@@ -1,15 +1,23 @@
 package com.example.clarp_returns;
 
 public class NoteItem {
-	String name;
-	Boolean isChecked;
-	String notes;
-	int type;
-	
-	public NoteItem(String name, int type){
-		this.name = name;
-		this.type = type;
-		notes = "";
-		isChecked = false;
-	}
+    String name;
+    Boolean isChecked;
+    String notes;
+    int type;
+
+    public NoteItem(String name, int type){
+        this.name = name;
+        this.type = type;
+        notes = "";
+        isChecked = false;
+    }
+
+    public void setChecked(boolean isChecked) {
+        this.isChecked = isChecked;
+    }
+
+    public boolean getChecked() {
+        return this.isChecked;
+    }
 }
--- clarp_returns/src/com/example/clarp_returns/NotesFragment.java
+++ clarp_returns/src/com/example/clarp_returns/NotesFragment.java
@@ -2,54 +2,54 @@ package com.example.clarp_returns;
 
 import java.util.ArrayList;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ListView;
 
 public class NotesFragment extends Fragment {
-	ArrayList<NoteItem> items;
-	public NoteItemAdapter adapter;
-	ListView listView;
-	
-	public NotesFragment(ArrayList<NoteItem> items){
-		super();
-		this.items = items;
-	}
-	
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_notes, container, false);
+    ArrayList<NoteItem> items;
+    public NoteItemAdapter adapter;
+    ListView listView;
 
-		listView = (ListView) rootView.findViewById(R.id.listNotes);
-		listView.setItemsCanFocus(true);
-		adapter = new NoteItemAdapter(getActivity(), android.R.layout.simple_list_item_1,items);
-		listView.setAdapter(adapter);
-		return rootView;
-	}
-	
-	
-	public void add(NoteItem newItem){
-		adapter.add(newItem);
-	}
-	
-	public int getNotesSize()
-	{
-		return adapter.getCount();
-	}
-	
-	public NoteItemAdapter getAdapter(){
-		return adapter;
-	}
-	
-	
+    public NotesFragment() {
+        super();
+    }
+
+    public NotesFragment(ArrayList<NoteItem> items){
+        super();
+        this.items = items;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_notes, container, false);
+
+        listView = (ListView) rootView.findViewById(R.id.listNotes);
+        listView.setItemsCanFocus(true);
+        adapter = new NoteItemAdapter(getActivity(), android.R.layout.simple_list_item_1,items);
+        listView.setAdapter(adapter);
+        return rootView;
+    }
+
+
+    public void add(NoteItem newItem){
+        adapter.add(newItem);
+    }
+
+    public int getNotesSize()
+    {
+        return adapter.getCount();
+    }
+
+    public NoteItemAdapter getAdapter(){
+        return adapter;
+    }
+
+
+
+
 }
